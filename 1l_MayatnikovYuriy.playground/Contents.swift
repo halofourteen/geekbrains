0ea9c03b5aabeb39eec5b4eb9373c269cfc1b5e2
Yuriy Mayatnikov <ymayatnikov@gmail.com>
2020-06-30 07:30:27 +0300

    Домашняя работа 7

--- 1l_MayatnikovYuriy.playground/Contents.swift
+++ 1l_MayatnikovYuriy.playground/Contents.swift
@@ -1,143 +1,117 @@
 import UIKit
 import PlaygroundSupport
 
-enum CarManufacturing {
-    case vaz, gaz
+enum BakeryShopError : Error {
+    case notInMenu
+    case outOfStock
+    case insufficientFunds(coinsNeeded: Double)
 }
 
-enum CarAction {
-    case windowOpen, windowClose, engineOn, engineOff
+enum Bakery {
+    case dunkin, mrdonut
 }
 
-protocol CarActionExecution {
-    var isWindowOpen: Bool {get set}
-    var isEngineOn: Bool {get set}
+struct Product {
+    var price: Double
+    var count: UInt
+    var bakeType: Bakery
 }
 
-extension CarActionExecution {
-    mutating func carActionExecution(action: CarAction) {
-        switch action {
-        case .windowOpen :
-            guard !isWindowOpen else {
-                print("Окна уже открыты")
-                break
-            }
-            isWindowOpen = true
-            print("Окна открыты")
-        case .windowClose :
-            guard isWindowOpen else {
-                print("Окна уже закрыты")
-                break
-            }
-            isWindowOpen = false
-            print("Окна закрыты")
-        case .engineOn :
-            guard !isEngineOn else {
-            print("Двигатель уже работает")
-            break
+class BakeryShop {
+    var product: [String: Product] = [
+        "Croissant": Product(price: 50, count: 0, bakeType: .dunkin),
+        "Donut": Product(price: 40, count: 5, bakeType: .mrdonut)
+    ]
+    
+    var funds: Double = 0
+    
+    private func tryToBuy(_ item: String) throws -> Bool {
+        guard var bakeType = product[item] else {
+            throw BakeryShopError.notInMenu
         }
-            isEngineOn = true
-            print("Двигатель работает")
-        case .engineOff :
-            guard isEngineOn else {
-            print("Двигатель уже остановлен")
-            break
+        guard funds >= bakeType.price else {
+            throw BakeryShopError.insufficientFunds(coinsNeeded: bakeType.price - funds)
         }
-            isEngineOn = false
-            print("Двигатель остановлен")
+        guard bakeType.count > 0 else {
+            throw BakeryShopError.outOfStock
         }
+        funds -= bakeType.price
+        bakeType.count -= 1
+        
+        return true
     }
-}
-
-enum SportCarAction {
-    case speedUp(speed: Int)
-}
-
-protocol SportCarActionExecution {
-    var speed: Int {get set}
-}
-
-extension SportCarActionExecution {
-    mutating func sportCarActionExecution(action: SportCarAction) {
-        switch action {
-        case let .speedUp(speed) :
-            self.speed += speed
-            print("Скорость \(self.speed)")
+    
+    func buy(_ item: String) -> String {
+        do {
+            try bakery.tryToBuy(item)
+            return "Вы купили \(item)"
+        } catch let BakeryShopError.insufficientFunds(coinsNeeded) {
+            return "Не хватает еще \(coinsNeeded)"
+        } catch BakeryShopError.outOfStock {
+            return "Товар закончился"
+        } catch BakeryShopError.notInMenu {
+            return "Нет в меню"
+        } catch let error {
+            return error.localizedDescription
         }
     }
 }
 
-enum TrunkCarAction {
-    case trunkLoad(weight: Int)
-    case trunkUnload(weight: Int)
-}
+var bakery: BakeryShop = BakeryShop()
 
-protocol TrunkCarActionExecution {
-    var trunkValueMax: Int {get set}
-    var trunkValue: Int {get set}
-}
+bakery.funds = 50
 
-extension TrunkCarActionExecution {
-    mutating func trunkCarActionExecution(action: TrunkCarAction) {
-        switch action {
-        case let .trunkLoad(weight) :
-            guard trunkValueMax > trunkValue + weight else {
-                print("Нехватает места, свободно \(trunkValueMax - trunkValue)")
-                break
-            }
-            trunkValue += weight
-            print("Успешно погружено \(trunkValue)")
-        case let .trunkUnload(weight) :
-            guard trunkValue - weight > 0 else {
-                print("В багажнике груза меньше, чем выгружается \(trunkValue)")
-                break
-            }
-            trunkValue -= weight
-            print("Успешно выгружено \(trunkValue)")
+print(bakery.buy("Croissant"))
+print(bakery.buy("Donut"))
+print(bakery.buy("Cheesecake"))
+
+class NewBakeryShop {
+    var product: [String: Product] = [
+        "Croissant": Product(price: 50, count: 0, bakeType: .dunkin),
+        "Donut": Product(price: 40, count: 5, bakeType: .mrdonut)
+    ]
+    
+    var funds: Double = 0
+    
+    func buy(_ item: String) -> (String?, BakeryShopError?) {
+        guard var productItem = product[item] else {
+            return (nil, BakeryShopError.notInMenu)
         }
+        guard funds >= productItem.price else {
+            return (nil, BakeryShopError.insufficientFunds(coinsNeeded: productItem.price - funds))
+        }
+        guard productItem.count > 0 else {
+            return (nil, BakeryShopError.outOfStock)
+        }
+        funds -= productItem.price
+        productItem.count -= 1
+        
+        return (item, nil)
     }
 }
 
-class SportCar: CarActionExecution, SportCarActionExecution {
-    var isWindowOpen: Bool = false
-    var isEngineOn: Bool = false
-    var speed: Int = 0
-    let manufacturing: CarManufacturing
-    init(manufacturing: CarManufacturing) {
-        self.manufacturing = manufacturing
-    }
+var newBakery: NewBakeryShop = NewBakeryShop()
+
+newBakery.funds = 100
+
+let sell1 = newBakery.buy("Croissant")
+let sell2 = newBakery.buy("Donut")
+let sell3 = newBakery.buy("Cheesecake")
+
+if let product = sell1.0 {
+    print("Вы купили: \(product)")
+} else if let error = sell1.1 {
+    print("Произошла ошибка: \(error)")
 }
 
-extension SportCar: CustomStringConvertible {
-    var description: String {
-        return "Состояние двигателя: \(isEngineOn) Состояние окон: \(isWindowOpen) Текущая скорость: \(speed) Производитель: \(manufacturing) "
-    }
+if let product = sell2.0 {
+    print("Вы купили: \(product)")
+} else if let error = sell2.1 {
+    print("Произошла ошибка: \(error)")
 }
 
-class TrunkCar: CarActionExecution, TrunkCarActionExecution {
-    var isWindowOpen: Bool = false
-    var isEngineOn: Bool = false
-    var trunkValueMax: Int
-    var trunkValue: Int = 0
-    let manufacturing: CarManufacturing
-    init(manufacturing: CarManufacturing, trunkValueMax: Int) {
-        self.manufacturing = manufacturing
-        self.trunkValueMax = trunkValueMax
-    }
+if let product = sell3.0 {
+    print("Вы купили: \(product)")
+} else if let error = sell3.1 {
+    print("Произошла ошибка: \(error)")
 }
-
-extension TrunkCar: CustomStringConvertible {
-    var description: String {
-        return "Состояние двигателя: \(isEngineOn) Состояние окон: \(isWindowOpen) Максимальный обьем багажника: \(trunkValueMax) Заполненный обьем багажника: \(trunkValue) Производитель: \(manufacturing) "
-    }
-}
-
-var sportCar: SportCar = SportCar(manufacturing: .vaz)
-sportCar.carActionExecution(action: .engineOn)
-sportCar.sportCarActionExecution(action: .speedUp(speed: 20))
-print(sportCar)
-
-var trunkCar: TrunkCar = TrunkCar(manufacturing: .gaz, trunkValueMax: 100)
-trunkCar.carActionExecution(action: .engineOff)
-trunkCar.trunkCarActionExecution(action: .trunkLoad(weight: 50))
-print(trunkCar)
